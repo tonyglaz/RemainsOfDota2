6c08b12eac7a7eb07397a4535cf015b0d03155e8
tonyglaz <tonyglazunov@gmail.com>
2023-02-28 10:47:16 +0300

    russified admin panel (meta classes added) + fixed some errors

--- teams/models.py
+++ teams/models.py
@@ -4,16 +4,27 @@ from django.db import models
 class Tournament(models.Model):
     slug = models.SlugField(max_length=150)
     name = models.CharField(max_length=150)
-    description = models.TextField(max_length=1000)
-    prize_pool = models.IntegerField(10)
-    number_of_teams = models.IntegerField(8)
+    description = models.TextField(max_length=1000, null=True)
+    prize_pool = models.IntegerField(verbose_name="Призовой фонд")
+    number_of_teams = models.IntegerField(verbose_name="Количество команд")
+
+    class Meta:
+        verbose_name = "Турнир"
+        verbose_name_plural = "Турниры"
+
+    def __str__(self):
+        return self.slug
 
 
 class Team(models.Model):
     slug = models.SlugField(max_length=50)
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=1000)
-    tournament = models.ManyToManyField(Tournament, null=True)
+    tournaments = models.ManyToManyField(Tournament)
+
+    class Meta:
+        verbose_name = "Команда"
+        verbose_name_plural = "Команды"
 
     def __str__(self):
         return self.slug
@@ -31,6 +42,10 @@ class Player(models.Model):
     nickname = models.CharField(max_length=100)
     status = models.CharField(max_length=11, choices=STATUS, default='WithoutTeam')
     team = models.ForeignKey(Team, on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        verbose_name = "Игрок"
+        verbose_name_plural = "Игроки"
 
     def __str__(self):
         return self.slug
