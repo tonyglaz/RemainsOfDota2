22ef8bd42bb65e01a66dddc670b79e50f3322334
tonyglaz <tonyglazunov@gmail.com>
2023-03-03 16:20:42 +0300

    started work with drf

--- RemainsOfDota2/urls.py
+++ RemainsOfDota2/urls.py
@@ -5,6 +5,7 @@ import teams.views as views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/',include('rest_framework.urls')),
     path('hello/', views.hello),
     path('player/', include('teams.urls'))
 ]
