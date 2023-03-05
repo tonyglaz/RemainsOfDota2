c030b92bb602f1560db272fb10bf86afe13f8249
tonyglaz <tonyglazunov@gmail.com>
2023-03-05 16:26:48 +0300

    generic_views drf for player

--- teams/serializers.py
+++ teams/serializers.py
@@ -1,29 +1,73 @@
 from rest_framework import serializers
 
-from teams.models import Player
+from teams.models import Player, Team
 
 
 class PlayerSerializer(serializers.ModelSerializer):
-    team=serializers.SlugRelatedField(
+    team = serializers.SlugRelatedField(
         read_only=True,
         slug_field='name'
     )
+
     class Meta:
         model = Player
-        fields = ['nickname', 'team']
+        fields = ['nickname', 'team', 'status', 'slug']
 
 
 class PlayerDetailSerializer(serializers.ModelSerializer):
     team = serializers.SlugRelatedField(
         read_only=True,
-        slug_field='name'
+        slug_field='name',
     )
+
     class Meta:
         model = Player
         fields = '__all__'
 
 
 class PlayerCreateSerializer(serializers.ModelSerializer):
+    team = serializers.SlugRelatedField(
+        required=False,
+        queryset=Team.objects.all(),
+        slug_field='name',
+    )
+
     class Meta:
-        model=Player
-        exclude=['id']
+        model = Player
+        fields = '__all__'
+
+    def is_valid(self, *, raise_exception=False):
+        self._team = self.initial_data.pop("team")
+        return super().is_valid(raise_exception=raise_exception)
+
+    def create(self, validated_data):
+        player = Player.objects.create(**validated_data)
+        player.save()
+        return player
+
+
+class PlayerUpdateSerializer(serializers.ModelSerializer):
+    team = serializers.SlugRelatedField(
+        required=False,
+        queryset=Team.objects.all(),
+        slug_field='name',
+    )
+
+    class Meta:
+        model = Player
+        fields = ['nickname', 'team', 'status', 'slug']
+
+    def is_valid(self, *, raise_exception=False):
+        self._team = self.initial_data.pop("team")
+        return super().is_valid(raise_exception=raise_exception)
+
+    def save(self):
+        player = super().save()
+        player.save()
+        return player
+
+
+class PlayerDestroySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = ['id']
--- teams/views.py
+++ teams/views.py
@@ -1,123 +1,120 @@
 import json
 
-from django.core.paginator import Paginator
 from django.http import HttpResponse, JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import UpdateView, DeleteView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 
-from django.forms.models import model_to_dict
-
-from RemainsOfDota2 import settings
 from teams.models import Player
-from teams.serializers import PlayerSerializer, PlayerDetailSerializer, PlayerCreateSerializer
+from teams.serializers import PlayerSerializer, PlayerDetailSerializer, PlayerCreateSerializer, PlayerUpdateSerializer, \
+    PlayerDestroySerializer
 
 
 def hello(request):
     return HttpResponse("Hello world")
 
 
-class PlayerView(ListView):
-    model = Player
-
-    def get(self, request, *args, **kwargs):
-        super().get(request, *args, **kwargs)  # у self появляется object_list где есть все игроки
-        search_nickname = request.GET.get('nickname', None)
-        if search_nickname:
-            self.object_list = self.object_list.filter(nickname=search_nickname)
-
-        self.object_list=self.object_list.order_by('team')
-
-        paginator=Paginator(self.object_list,settings.TOTAL_ON_PAGE)
-        page_number=request.GET.get("page")
-        page_obj=paginator.get_page(page_number)
-
-        # players=[]
-        # for player in page_obj:
-        #     players.append({
-        #         'id': player.id,
-        #         'nickname': player.nickname,
-        #         'team': player.team.name,
-        #         'status': player.status
-        #     })
-
-       # list(map(lambda x: setattr(x,"team",x.team.name if x.team else None),page_obj))
-        response = {
-            "items":PlayerSerializer(page_obj,many=True).data,
-            "num_pages":paginator.num_pages,
-            "total":paginator.count
-        }
-
-        return JsonResponse(response, safe=False)
-
-
-class PlayerDetailView(DetailView):
-    model = Player
-
-    def get(self, request, *args, **kwargs):
-        player = self.get_object()
-
-        return JsonResponse(PlayerDetailSerializer(player).data)
+class PlayerView(ListAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+    # def get(self, request, *args, **kwargs):
+    #     super().get(request, *args, **kwargs)  # у self появляется object_list где есть все игроки
+    #     search_nickname = request.GET.get('nickname', None)
+    #     if search_nickname:
+    #         self.object_list = self.object_list.filter(nickname=search_nickname)
+    #
+    #     self.object_list=self.object_list.order_by('team')
+    #
+    #     paginator=Paginator(self.object_list,settings.TOTAL_ON_PAGE)
+    #     page_number=request.GET.get("page")
+    #     page_obj=paginator.get_page(page_number)
+    #
+    #     # players=[]
+    #     # for player in page_obj:
+    #     #     players.append({
+    #     #         'id': player.id,
+    #     #         'nickname': player.nickname,
+    #     #         'team': player.team.name,
+    #     #         'status': player.status
+    #     #     })
+    #
+    #    # list(map(lambda x: setattr(x,"team",x.team.name if x.team else None),page_obj))
+    #     response = {
+    #         "items":PlayerSerializer(page_obj,many=True).data,
+    #         "num_pages":paginator.num_pages,
+    #         "total":paginator.count
+    #     }
+    #
+    #     return JsonResponse(response, safe=False)
 
 
-@method_decorator(csrf_exempt, name='dispatch')
-class PlayerCreateView(CreateView):
-    model = Player
-    fields = ['slug', 'nickname', 'status', 'team']
+class PlayerDetailView(RetrieveAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerDetailSerializer
 
-    def post(self, request):
-        player_data = PlayerCreateSerializer(data=json.loads(request.body))
-        if player_data.is_valid():
-            player_data.save()
-        else:
-            return JsonResponse(player_data.errors)
-        # player = Player.objects.create(
-        #     #  player_id=player_data['player_id'],
-        #     slug=player_data['slug'],
-        #     nickname=player_data['nickname'],
-        #     status=player_data['status'],
-        #     team=player_data['team']
-        # )
-        #
-        # player.save()
-        return JsonResponse(player_data.data)
-
-@method_decorator(csrf_exempt, name='dispatch')
-class PlayerUpdateView(UpdateView):
-    model = Player
-    fields = ['slug', 'nickname', 'status', 'team']
-
-    def patch(self, request, *args, **kwargs):
-        super().post(request, *args, **kwargs)
-
-        player_data = json.loads(request.body)
-        self.slug = player_data['slug']
-        self.nickname = player_data['nickname']
-        self.status = player_data['status']
-        self.team.name = player_data['team']
-
-        # for skill in player_data:
-        #     try:
-        #         skill_obj=Player.objects.get(name=skill)
-        #     except Player.DoesNotExist:
-        #         return JsonResponse({"error":"skill not found"})
-        #     self.object.player.add(skill_obj)
-        #  self.team = player_data['team']
-        return JsonResponse({
-            #   'id': self.object.id,
-            'nickname': self.object.nickname,
-            'status': self.object.status,
-            'team': self.object.team.name,
-            'slug': self.object.slug,
-        }, safe=False)
+    # def get(self, request, *args, **kwargs):
+    #     player = self.get_object()
+    #
+    #     return JsonResponse(PlayerDetailSerializer(player).data)
 
 
-@method_decorator(csrf_exempt, name='dispatch')
-class PlayerDeleteView(DeleteView):
-    model = Player
-    success_url = '/'
+class PlayerCreateView(CreateAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerCreateSerializer
 
-    def delete(self, request, *args, **kwargs):
-        super().delete(request, *args, **kwargs)
+    # def post(self, request):
+    #     player_data = PlayerCreateSerializer(data=json.loads(request.body))
+    #     if player_data.is_valid():
+    #         player_data.save()
+    #     else:
+    #         return JsonResponse(player_data.errors)
+    #     # player = Player.objects.create(
+    #     #     #  player_id=player_data['player_id'],
+    #     #     slug=player_data['slug'],
+    #     #     nickname=player_data['nickname'],
+    #     #     status=player_data['status'],
+    #     #     team=player_data['team']
+    #     # )
+    #     #
+    #     # player.save()
+    #     return JsonResponse(player_data.data)
 
-        return JsonResponse({"status": "ok"}, status=200)
+
+class PlayerUpdateView(UpdateAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerUpdateSerializer
+
+    # def patch(self, request, *args, **kwargs):
+    #     super().post(request, *args, **kwargs)
+    #
+    #     player_data = json.loads(request.body)
+    #     self.slug = player_data['slug']
+    #     self.nickname = player_data['nickname']
+    #     self.status = player_data['status']
+    #     self.team.name = player_data['team']
+    #
+    #     # for skill in player_data:
+    #     #     try:
+    #     #         skill_obj=Player.objects.get(name=skill)
+    #     #     except Player.DoesNotExist:
+    #     #         return JsonResponse({"error":"skill not found"})
+    #     #     self.object.player.add(skill_obj)
+    #     #  self.team = player_data['team']
+    #     return JsonResponse({
+    #         #   'id': self.object.id,
+    #         'nickname': self.object.nickname,
+    #         'status': self.object.status,
+    #         'team': self.object.team.name,
+    #         'slug': self.object.slug,
+    #     }, safe=False)
+
+
+class PlayerDeleteView(DestroyAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerDestroySerializer
+
+    # def delete(self, request, *args, **kwargs):
+    #     super().delete(request, *args, **kwargs)
+    #
+    #     return JsonResponse({"status": "ok"}, status=200)
