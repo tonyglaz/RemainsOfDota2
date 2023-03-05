5e793771ef5d3d6e421a56127d8c36b996fd70f2
tonyglaz <tonyglazunov@gmail.com>
2023-03-05 18:31:27 +0300

    drf pagination

--- RemainsOfDota2/settings.py
+++ RemainsOfDota2/settings.py
@@ -123,4 +123,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-TOTAL_ON_PAGE = 6
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
